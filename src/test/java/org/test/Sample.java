42cde3d8a25429f80f2ea553c76654ad678040db
dhineshkumarmech <dhineshkumarmech1998@gmail.com>
2022-01-31 14:20:22 +0530

    kumar's code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -5,8 +5,8 @@ public class Sample {
 	public static void main(String[] args) {
 		
 		System.out.println("Test1");
-		System.out.println("Test4");
-		System.out.println("Test5");
+		System.out.println("Test2");
+		System.out.println("Test3");
 		
 	}
 
